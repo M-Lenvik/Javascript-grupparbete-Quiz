8db3ffa9a5a66f7e60fee8fccb50b1a50976e0a6
M-Lenvik <lenvik.marie@gmail.com>
2024-12-27 21:09:45 +0100

    added: 10 till frågor

--- data/quizQuestions.ts
+++ data/quizQuestions.ts
@@ -94,7 +94,96 @@ export const quizQuestions: Question[] = [
             "C) Madagaskar"
         ],
         correctAnswer: "A) Shrek"
+    },
+    {
+        question: "Vilken film regisserades av Steven Spielberg och handlar om en dinosauriepark?",
+        options: [
+            "A) Jurassic Park", 
+            "B) King Kong", 
+            "C) Avatar"
+        ],
+        correctAnswer: "A) Jurassic Park"
+    },
+    {
+        question: "Vem spelade rollen som Jack Dawson i filmen Titanic?",
+        options: [
+            "A) Johnny Depp", 
+            "B) Leonardo DiCaprio", 
+            "C) Tom Cruise"
+        ],
+        correctAnswer: "B) Leonardo DiCaprio"
+    },
+    {
+        question: "Vilken film vann pris för Bästa Film på Oscarsgalan 2020?",
+        options: [
+            "A) 1917", 
+            "B) Parasite", 
+            "C) Joker"
+        ],
+        correctAnswer: "B) Parasite"
+    },
+    {
+        question: "Vad heter huvudkaraktären i The Matrix, spelad av Keanu Reeves?",
+        options: [
+            "A) Neo", 
+            "B) Morpheus", 
+            "C) Smith"
+        ],
+        correctAnswer: "A) Neo"
+    },
+    {
+        question: "Vilken film är den första i Star Wars-serien som släpptes på bio?",
+        options: [
+            "A) Episode I - The Phantom Menace", 
+            "B) Episode IV - A New Hope", 
+            "C) Episode VI - Return of the Jedi"
+        ],
+        correctAnswer: "B) Episode IV - A New Hope"
+    },
+    {
+        question: "I vilken film säger karaktären Hannibal Lecter den kända repliken: “Hello, Clarice”?",
+        options: [
+            "A) Psycho", 
+            "B) The Shining", 
+            "C) The Silence of the Lambs"
+        ],
+        correctAnswer: "C) The Silence of the Lambs"
+    },
+    {
+        question: "Vilken film vann Oscar för bästa film 1994?",
+        options: [
+            "A) Forrest Gump", 
+            "B) Pulp Fiction", 
+            "C) The Shawshank Redemption"
+        ],
+        correctAnswer: "B) Forrest Gump"
+    },
+    {
+        question: "Vad heter den blå huvudkaraktären i filmen Avatar?",
+        options: [
+            "A) Jake Sully", 
+            "B) Neytiri", 
+            "C) Tsu'Tey"
+        ],
+        correctAnswer: "A) Jake Sully"
+    },
+    {
+        question: "Vilken animerad film handlar om en råtta som drömmer om att bli kock i Paris?",
+        options: [
+            "A) Ratatouille", 
+            "B) Zootropolis", 
+            "C) Bilar"
+        ],
+        correctAnswer: "A) Ratatouille"
+    },
+    {
+        question: "I vilken film hittar Frodo Baggins en mäktig ring?",
+        options: [
+            "A) The Hobbit", 
+            "B) Sagan om Ringen: Härskarringen", 
+            "C) Narnia: Häxan och Lejonet"
+        ],
+        correctAnswer: "B) Sagan om Ringen: Härskarringen"
     }
 ];
-
 export default quizQuestions;
